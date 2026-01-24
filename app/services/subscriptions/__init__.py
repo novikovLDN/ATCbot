fab62e38b3524be489956d82b6df7d881261e186
Maximilian <mr.maxlin@icloud.com>
2026-01-24 19:14:22 +0300

    refactor: finalize handler cleanup and move all subscription/trial logic to services

--- app/services/subscriptions/__init__.py
+++ app/services/subscriptions/__init__.py
@@ -1,1 +1,39 @@
-# Subscriptions service package
+"""
+Subscription Service Package
+"""
+
+from app.services.subscriptions.service import (
+    calculate_price,
+    create_purchase,
+    finalize_purchase,
+    calculate_renewal_price,
+    renew_subscription,
+    is_subscription_active,
+    get_subscription_status,
+    parse_expires_at,
+    check_and_disable_expired_subscription,
+    SubscriptionStatus,
+    SubscriptionServiceError,
+    InvalidTariffError,
+    PriceCalculationError,
+    PurchaseCreationError,
+    PaymentFinalizationError,
+)
+
+__all__ = [
+    "calculate_price",
+    "create_purchase",
+    "finalize_purchase",
+    "calculate_renewal_price",
+    "renew_subscription",
+    "is_subscription_active",
+    "get_subscription_status",
+    "parse_expires_at",
+    "check_and_disable_expired_subscription",
+    "SubscriptionStatus",
+    "SubscriptionServiceError",
+    "InvalidTariffError",
+    "PriceCalculationError",
+    "PurchaseCreationError",
+    "PaymentFinalizationError",
+]
